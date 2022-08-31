11703ddd5499c76f0b98045b746e2c55fe4c2080
Halalisani <halalisani@iamsignific.com>
2022-08-31 16:09:41 +0200

    fix error message styling

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -3,10 +3,15 @@ import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useTeams from '../hooks/useTeams';
 import GridView from '../components/GridView';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Raleway_500Medium } from '@expo-google-fonts/raleway';
 
 const SearchScreen = () => {
   const [searchText, setSearchText] = useState('');
   const [errorMsg, teamOneDetails, teamTwoDetails, teamOnePlayers, teamTwoPlayers, filteredResults, filteredResultsTeam, searchPlayer] = useTeams();
+  let [fontsLoaded, error] = useFonts({ Raleway_500Medium });
+
+  if (!fontsLoaded) { return <AppLoading /> }
 
   return (
     <View style={styles.backgroundStyle}>
@@ -15,7 +20,12 @@ const SearchScreen = () => {
         onSearchChange={(searchText: string) => { setSearchText(searchText); searchPlayer(searchText) }}
         onSearchSubmit={() => searchPlayer(searchText)}
       />
-      {errorMsg && (<Text>{`${errorMsg}`}</Text>)}
+
+      {errorMsg && (
+        <View style={styles.errorMessageContainer}>
+          <Text style={styles.errorMessage}>{`${errorMsg}`}</Text>
+        </View>
+      )}
 
       <GridView
         searchText={searchText}
@@ -34,6 +44,16 @@ const styles = StyleSheet.create({
   backgroundStyle: {
     flex: 1,
     backgroundColor: '#fff'
+  },
+  errorMessageContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  errorMessage: {
+    fontSize: 18,
+    fontWeight: '700',
+    fontFamily: 'Raleway_500Medium'
   }
 });
 
